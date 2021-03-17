05e6eba7818ca81960295585f9ab88abb96198c1
Artem <artemkartunchikov@mail.ru>
2021-03-17 10:05:32 +0300

    push 345 again

---
+++ IT_pr_345/Form1.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace IT_pr_4
+{
+  partial class Form1
+  {
+    /// <summary>
+    /// Обязательная переменная конструктора.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Освободить все используемые ресурсы.
+    /// </summary>
+    /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Код, автоматически созданный конструктором форм Windows
+
+    /// <summary>
+    /// Требуемый метод для поддержки конструктора — не изменяйте 
+    /// содержимое этого метода с помощью редактора кода.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.textBox1 = new System.Windows.Forms.TextBox();
+      this.groupBox1 = new System.Windows.Forms.GroupBox();
+      this.radioButton3 = new System.Windows.Forms.RadioButton();
+      this.radioButton2 = new System.Windows.Forms.RadioButton();
+      this.radioButton1 = new System.Windows.Forms.RadioButton();
+      this.textBox2 = new System.Windows.Forms.TextBox();
+      this.label1 = new System.Windows.Forms.Label();
+      this.button1 = new System.Windows.Forms.Button();
+      this.radioButton4 = new System.Windows.Forms.RadioButton();
+      this.listBox1 = new System.Windows.Forms.ListBox();
+      this.radioButton5 = new System.Windows.Forms.RadioButton();
+      this.label2 = new System.Windows.Forms.Label();
+      this.groupBox1.SuspendLayout();
+      this.SuspendLayout();
+      // 
+      // textBox1
+      // 
+      this.textBox1.Location = new System.Drawing.Point(26, 193);
+      this.textBox1.Multiline = true;
+      this.textBox1.Name = "textBox1";
+      this.textBox1.Size = new System.Drawing.Size(180, 83);
+      this.textBox1.TabIndex = 0;
+      // 
+      // groupBox1
+      // 
+      this.groupBox1.Controls.Add(this.radioButton5);
+      this.groupBox1.Controls.Add(this.radioButton4);
+      this.groupBox1.Controls.Add(this.radioButton3);
+      this.groupBox1.Controls.Add(this.radioButton2);
+      this.groupBox1.Controls.Add(this.radioButton1);
+      this.groupBox1.Location = new System.Drawing.Point(30, 338);
+      this.groupBox1.Name = "groupBox1";
+      this.groupBox1.Size = new System.Drawing.Size(162, 100);
+      this.groupBox1.TabIndex = 1;
+      this.groupBox1.TabStop = false;
+      this.groupBox1.Text = " Практика";
+      // 
+      // radioButton3
+      // 
+      this.radioButton3.AutoSize = true;
+      this.radioButton3.Location = new System.Drawing.Point(7, 66);
+      this.radioButton3.Name = "radioButton3";
+      this.radioButton3.Size = new System.Drawing.Size(42, 17);
+      this.radioButton3.TabIndex = 2;
+      this.radioButton3.Text = "exp";
+      this.radioButton3.UseVisualStyleBackColor = true;
+      this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+      // 
+      // radioButton2
+      // 
+      this.radioButton2.AutoSize = true;
+      this.radioButton2.Location = new System.Drawing.Point(6, 43);
+      this.radioButton2.Name = "radioButton2";
+      this.radioButton2.Size = new System.Drawing.Size(37, 17);
+      this.radioButton2.TabIndex = 1;
+      this.radioButton2.Text = "ch";
+      this.radioButton2.UseVisualStyleBackColor = true;
+      this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+      // 
+      // radioButton1
+      // 
+      this.radioButton1.AutoSize = true;
+      this.radioButton1.Checked = true;
+      this.radioButton1.Location = new System.Drawing.Point(7, 20);
+      this.radioButton1.Name = "radioButton1";
+      this.radioButton1.Size = new System.Drawing.Size(36, 17);
+      this.radioButton1.TabIndex = 0;
+      this.radioButton1.TabStop = true;
+      this.radioButton1.Text = "sh";
+      this.radioButton1.UseVisualStyleBackColor = true;
+      this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+      // 
+      // textBox2
+      // 
+      this.textBox2.Location = new System.Drawing.Point(26, 312);
+      this.textBox2.Name = "textBox2";
+      this.textBox2.Size = new System.Drawing.Size(166, 20);
+      this.textBox2.TabIndex = 2;
+      // 
+      // label1
+      // 
+      this.label1.AutoSize = true;
+      this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+      this.label1.Location = new System.Drawing.Point(23, 279);
+      this.label1.Name = "label1";
+      this.label1.Size = new System.Drawing.Size(81, 17);
+      this.label1.TabIndex = 3;
+      this.label1.Text = "Введите  x:";
+      // 
+      // button1
+      // 
+      this.button1.Location = new System.Drawing.Point(198, 393);
+      this.button1.Name = "button1";
+      this.button1.Size = new System.Drawing.Size(166, 39);
+      this.button1.TabIndex = 4;
+      this.button1.Text = " Расчет";
+      this.button1.UseVisualStyleBackColor = true;
+      this.button1.Click += new System.EventHandler(this.button1_Click);
+      // 
+      // radioButton4
+      // 
+      this.radioButton4.AutoSize = true;
+      this.radioButton4.Location = new System.Drawing.Point(67, 20);
+      this.radioButton4.Name = "radioButton4";
+      this.radioButton4.Size = new System.Drawing.Size(81, 17);
+      this.radioButton4.TabIndex = 3;
+      this.radioButton4.Text = "4 практика";
+      this.radioButton4.UseVisualStyleBackColor = true;
+      this.radioButton4.CheckedChanged += new System.EventHandler(this.radioButton4_CheckedChanged);
+      // 
+      // listBox1
+      // 
+      this.listBox1.FormattingEnabled = true;
+      this.listBox1.Items.AddRange(new object[] {
+            "Последовательность знаков. как правило! состоящая из слов и пробелов?",
+            "Устанавливаемая. при форматировании! текста! и построенная? обычно по базовой.",
+            "(опорной) строке текста.",
+            "Длина строки зависит от места. отведенного. от. фиксирован!ной точки! при?"});
+      this.listBox1.Location = new System.Drawing.Point(212, 20);
+      this.listBox1.Name = "listBox1";
+      this.listBox1.Size = new System.Drawing.Size(453, 355);
+      this.listBox1.TabIndex = 5;
+      // 
+      // radioButton5
+      // 
+      this.radioButton5.AutoSize = true;
+      this.radioButton5.Location = new System.Drawing.Point(67, 43);
+      this.radioButton5.Name = "radioButton5";
+      this.radioButton5.Size = new System.Drawing.Size(81, 17);
+      this.radioButton5.TabIndex = 4;
+      this.radioButton5.Text = "5 практика";
+      this.radioButton5.UseVisualStyleBackColor = true;
+      this.radioButton5.CheckedChanged += new System.EventHandler(this.radioButton5_CheckedChanged);
+      // 
+      // label2
+      // 
+      this.label2.AutoSize = true;
+      this.label2.Location = new System.Drawing.Point(385, 393);
+      this.label2.Name = "label2";
+      this.label2.Size = new System.Drawing.Size(35, 13);
+      this.label2.TabIndex = 6;
+      this.label2.Text = "label2";
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(800, 450);
+      this.Controls.Add(this.label2);
+      this.Controls.Add(this.listBox1);
+      this.Controls.Add(this.button1);
+      this.Controls.Add(this.label1);
+      this.Controls.Add(this.textBox2);
+      this.Controls.Add(this.groupBox1);
+      this.Controls.Add(this.textBox1);
+      this.Name = "Form1";
+      this.Text = "Form1";
+      this.groupBox1.ResumeLayout(false);
+      this.groupBox1.PerformLayout();
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+    }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+    private System.Windows.Forms.Button button1;
+    private System.Windows.Forms.RadioButton radioButton4;
+    private System.Windows.Forms.RadioButton radioButton5;
+    private System.Windows.Forms.ListBox listBox1;
+    private System.Windows.Forms.Label label2;
+  }
+}
+
---
+++ IT_pr_345/Form1.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IT_pr_4
+{
+  public partial class Form1 : Form
+  {
+    public Form1()
+    {
+      InitializeComponent();
+      radioButton1.Checked = false;
+      label2.Visible = false;
+    }
+
+    private void radioButton1_CheckedChanged(object sender, EventArgs e)
+    {
+      Fx_switch = 1;
+      label1.Text = "Введите x";
+      textBox1.Visible = true;
+      label1.Visible = true;
+      textBox2.Visible = true;
+      listBox1.Visible = false;
+    }
+
+    private void radioButton2_CheckedChanged(object sender, EventArgs e)
+    {
+      Fx_switch = 2;
+      label1.Text = "Введите x";
+      textBox1.Visible = true;
+      label1.Visible = true;
+      textBox2.Visible = true;
+      listBox1.Visible = false;
+    }
+
+    private void radioButton3_CheckedChanged(object sender, EventArgs e)
+    {
+      Fx_switch = 3;
+      label1.Text = "Введите x";
+      textBox1.Visible = true;
+      label1.Visible = true;
+      textBox2.Visible = true;
+      listBox1.Visible = false;
+    }
+
+    int Fx_switch = 0;
+
+
+    public void pr_3(double x)
+    {
+      double y = 0;
+      switch (Fx_switch)
+      {
+        case 1:
+          y = Math.Sinh(x);
+          break;
+        case 2:
+          y = Math.Cosh(x);
+          break;
+        case 3:
+          y = Math.Exp(x);
+          break;
+      }
+      if ((x >= 0) && (x <= 3))
+      {
+        y = 2;
+      }
+      else if (x > 3)
+      { 
+        y = y * 0.5;
+      }
+      else if (x < 0)
+      {
+        y = 1 * y;
+      }
+
+      textBox1.Text = "При x = " + Convert.ToString(x) + Environment.NewLine + "y =" + Convert.ToString(y);
+
+    }
+
+    public double fact(int number)
+    {
+      if (number == 1)
+        return 1;
+      else
+        return number * fact(number - 1);
+
+      
+    }
+    public void pr_4(double eps)
+    {
+      double sum = 0;
+      double toch = eps+1;
+      int i = 1;
+      while (eps<toch)
+      {
+        toch = 2 / (Math.Sqrt(fact(i) + 4));
+        sum += toch;
+        
+        i++;
+      }
+      textBox1.Text = "При eps = " + Convert.ToString(eps) + Environment.NewLine + "sum =" + Convert.ToString(sum);
+    }
+
+    public void pr_5()
+    {
+      int index = listBox1.SelectedIndex;
+      string str = (string)listBox1.Items[index];
+      int len = str.Length;
+      int i = 0;
+      int count = 0;
+      while (i < len)
+      {
+        if ((str[i] == '.')|| (str[i] == '!')|| (str[i] == '?'))
+          count++;
+        i++;
+      }
+      label2.Visible = true;
+      label2.Text = "Колличеств предложений = " + Convert.ToString(count);
+    }
+
+    private void button1_Click(object sender, EventArgs e)
+    {
+      double x = 0;
+      if (Fx_switch == 5)
+      {
+        pr_5();
+      }
+      if (double.TryParse(textBox2.Text, out x))  
+      {
+        if (Fx_switch == 4)
+        {
+          pr_4(x);
+        }
+        else
+          pr_3(x);
+      }
+      else
+      {
+        textBox1.Text = "Ошибка ввода!  ";
+      }
+      
+    }
+
+    private void radioButton4_CheckedChanged(object sender, EventArgs e)
+    {
+      Fx_switch = 4;
+      label1.Text = "Введите точность";
+      textBox1.Visible = true;
+      label1.Visible = true;
+      textBox2.Visible = true;
+      listBox1.Visible = false;
+    }
+
+    private void radioButton5_CheckedChanged(object sender, EventArgs e)
+    {
+      Fx_switch = 5;
+      textBox1.Visible = false;
+      label1.Visible = false;
+      textBox2.Visible = false;
+      listBox1.Visible = true;
+    }
+  }
+}
