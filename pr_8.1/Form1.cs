49676ac265aab8150f5802898ddcbc8eee301ba9
madgentt <64005088+madgentt@users.noreply.github.com>
2021-04-05 15:50:08 +0300

    Add files via upload

---
+++ pr_8.1/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace pr_8
+{
+  public partial class Form1 : Form
+  {
+    private double XMin = 2;
+    private double XMax = 10;
+    private double Step = 0.5;
+    private double[] x;
+    private double[] y1;
+    private double[] y2;
+    Chart chart;
+
+    public Form1()
+    {
+      InitializeComponent();
+    }
+
+
+    private void CalcFunction()
+    {
+      int count = (int)Math.Ceiling((XMax - XMin) / Step) + 1;
+      x = new double[count];
+      y1 = new double[count];
+      y2 = new double[count];
+      for (int i = 0; i < count; i++)
+      {
+        x[i] = XMin + Step * i;
+        y1[i] = (((2*Math.Log(x[i])*Math.Cos(2*x[i]))-3*(Math.Pow((x[i]+1), 2))/(x[i] - 1))/(2+Math.Sqrt(x[i])));
+        y2[i] = Math.Cos(x[i]);
+      }
+    }
+
+
+    private void CreateChart()
+    {
+      chart = new Chart();
+      chart.Parent = this;
+      chart.SetBounds(10, 10, ClientSize.Width - 20, ClientSize.Height - 20);
+      ChartArea area = new ChartArea();
+      area.Name = "myGraph";
+      area.AxisX.Minimum = XMin;
+      area.AxisX.Maximum = XMax;
+      area.AxisX.MajorGrid.Interval = Step;
+      chart.ChartAreas.Add(area);
+
+      // Создаём объект для первого графика
+      Series series1 = new Series();
+      // Ссылаемся на область для построения графика
+      series1.ChartArea = "myGraph";
+      // Задаём тип графика - сплайны
+      series1.ChartType = SeriesChartType.Spline;
+      // Указываем ширину линии графика
+      series1.BorderWidth = 3;
+      // Название графика для отображения в легенде
+      // Добавляем в список графиков диаграммы
+      chart.Series.Add(series1);
+      // Аналогичные действия для второго графика
+      Series series2 = new Series();
+      series2.ChartArea = "myGraph";
+      series2.ChartType = SeriesChartType.Spline;
+      series2.BorderWidth = 3;
+      chart.Series.Add(series2);
+
+      // Создаёмлегенду, котораябудетпоказыватьназвания
+      Legend legend = new Legend();
+      chart.Legends.Add(legend);
+    }
+
+
+
+    private void Form1_Load(object sender, EventArgs e)
+    {
+      // Создаём элемент управления
+      CreateChart();
+
+      // Расчитываем значения точек графиков функций
+      CalcFunction();
+
+      // Добавляем вычисленные значения в графики
+      chart.Series[0].Points.DataBindXY(x, y1);
+    }
+
+
+
+  }
+
+}
---
+++ pr_9/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+using System.Threading;
+
+
+
+
+namespace pr_9
+{
+
+  public partial class Form1 : Form
+  {
+    public Form1()
+    {
+      InitializeComponent();
+    }
+
+    static int coefX = 1;
+    static int coefY = 1;
+    static int x = coefX * 150;
+    static int y = coefY * 20;
+    private void Form1_Paint(object sender, PaintEventArgs e)
+    {
+      Graphics g = e.Graphics;
+      g.Clear(Color.White);
+      button1.BringToFront();
+      g.FillRectangle(new SolidBrush(System.Drawing.ColorTranslator.FromHtml("#666666")), new Rectangle(100, 0, 400, 500));
+
+      
+      
+
+      g.FillRectangle(new SolidBrush(System.Drawing.ColorTranslator.FromHtml("#f3f3f3")), new Rectangle(275, 10, 40, 100));
+      g.FillRectangle(new SolidBrush(System.Drawing.ColorTranslator.FromHtml("#f3f3f3")), new Rectangle(275, 140, 40, 100));
+      g.FillRectangle(new SolidBrush(System.Drawing.ColorTranslator.FromHtml("#f3f3f3")), new Rectangle(275, 270, 40, 100));
+      g.FillRectangle(new SolidBrush(System.Drawing.ColorTranslator.FromHtml("#f3f3f3")), new Rectangle(275, 400, 40, 100));
+      g.FillRectangle(new SolidBrush(System.Drawing.ColorTranslator.FromHtml("#9900ff")), new Rectangle(x, y, 70, 150));
+
+      for (int i = 0; i < 10; i++)
+      {
+        g.FillRectangle(new SolidBrush(System.Drawing.ColorTranslator.FromHtml("#9900ff")), new Rectangle(i * 10 + x, i * 10 + y, 70, 150));
+        Thread.Sleep(500);
+        g.FillRectangle(new SolidBrush(System.Drawing.ColorTranslator.FromHtml("#666666")), new Rectangle(i * 10+x, i * 10 + +y, 70, 150));
+        
+        g.FillRectangle(new SolidBrush(System.Drawing.ColorTranslator.FromHtml("#9900ff")), new Rectangle((i+1)*10+x, (i+1) * 10 + y, 70, 150));
+      }
+
+      
+      
+
+
+
+
+      g.DrawLine(new Pen(Brushes.Black, 2), 10, 20, 500, 20);
+
+   
+    }
+
+    private void button1_Click(object sender, EventArgs e)
+    {
+      coefX = 2; 
+    }
+
+    private void button2_Click(object sender, EventArgs e)
+    {
+
+    }
+
+    private void button3_Click(object sender, EventArgs e)
+    {
+
+    }
+
+    private void button4_Click(object sender, EventArgs e)
+    {
+
+    }
+  }
+}
